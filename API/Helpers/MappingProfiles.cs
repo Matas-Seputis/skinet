6272833a780121f27c8aaf4e956de3d918699a3e
Matas Seputis <matasseputis@gmail.com>
2021-03-21 15:35:00 +0100

    API - Validation

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -14,6 +14,8 @@ namespace API.Helpers
                 .ForMember(d => d.ProductType, options => options.MapFrom(s => s.ProductType.Name))
                 .ForMember(d => d.PictureUrl, options => options.MapFrom<ProductUrlResolver>());
             CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<CustomerBasketDto, CustomerBasket>();
+            CreateMap<BasketItemDto, BasketItem>();
         }
     }
 }
